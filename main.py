a2d724ab5d92a1201078af38a8708fbf5499e9b3
Ganesh-Paudel <paudelganesh080@gmail.com>
2026-04-03 16:34:20 -0400

    Fixed the close window bug. excluded animate search calling from loop in main file and added user window close check in visualize.py

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from algorithms.aStar import AStar
 import pandas as pd
 
 def main():
-    graph = Graph('data/nodes.csv', 'data/edges.csv')
+    graph = Graph('data/testData/nodes.csv', 'data/testData/edges.csv')
     test = getTestData();
-    for source,target in zip(test[0],test[1]):
+    
+    for source,target in zip(test[0][1:],test[1][1:]):
         solve = AStar(graph);
         path = solve.solve(source, target)
-    # visual = GraphVisualizer(graph)
-    # visual.animate_search(solve.visited_nodes, path, source, target)
-        solve.print_performance();
-        solve.save_performance("performance.csv");
+        visual = GraphVisualizer(graph)
+
+    visual.animate_search(solve.visited_nodes, path, source, target)
+        # solve.print_performance();
+        # solve.save_performance("performance.csv");
 
 def getTestData():
-    data = pd.read_csv("data/testCases.csv", names = ['source', 'target'], dtype = {'source':str, 'target':str})
+    data = pd.read_csv("data/testData/testCases.csv", names = ['source', 'target'], dtype = {'source':str, 'target':str})
     source = data['source'].tolist();
     target = data['target'].tolist();
     return [source,target]
